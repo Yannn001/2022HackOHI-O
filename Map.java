8710c66c387fda5f873012c314198811b21fa8b6
Yixuan Huang <huang.4013@buckeyemail.osu.edu>
2022-10-08 12:42:51 -0400

    test

--- Map.java
+++ Map.java
@@ -1,9 +1,8 @@
 import java.util.*;
 
 public class Map {
-
     List<List<Map.Entry<k,v>>> route(String id, boolean canWalk, boolean ){
-
+        List<List<Map<k, v>>> allRoute = new List<>;
         return allRoute;
     }
 
