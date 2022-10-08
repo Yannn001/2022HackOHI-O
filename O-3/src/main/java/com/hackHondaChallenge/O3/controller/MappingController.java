31c62ffa1430e595654a7d49306bd4c9e26c4cba
chengyan <wang.12517@buckeyemail.osu.edu>
2022-10-08 14:01:25 -0400

    refined backend(should be good for now)

--- O-3/src/main/java/com/hackHondaChallenge/O3/controller/MappingController.java
+++ O-3/src/main/java/com/hackHondaChallenge/O3/controller/MappingController.java
@@ -2,7 +2,11 @@ package com.hackHondaChallenge.O3.controller;
 
 import com.hackHondaChallenge.O3.service.hackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/hack22")
@@ -11,9 +15,11 @@ public class MappingController {
     @Autowired
     private hackService studentService;
 
-    @GetMapping("/compute")
-    public Integer add() {
-        int a = studentService.compute(99);
-        return a;
+    @GetMapping("/compute/{id}")
+    public Integer add(@PathVariable Integer id) {
+
+            int a = studentService.compute(id);
+            return a;
+
     }
 }
--- O-3/src/main/java/com/hackHondaChallenge/O3/service/hackServiceImp.java
+++ O-3/src/main/java/com/hackHondaChallenge/O3/service/hackServiceImp.java
@@ -2,7 +2,7 @@ package com.hackHondaChallenge.O3.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentServiceImp implements hackService {
+public class hackServiceImp implements hackService {
     @Override
     public Integer compute(Integer i) {
         return i;
