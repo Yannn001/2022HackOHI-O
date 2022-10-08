d010425b73f10fb738cbb6fffa76c7e48cc6eaa1
Yixuan Huang <huang.4013@buckeyemail.osu.edu>
2022-10-08 17:18:44 -0400

    Add functions

--- O-3/src/main/java/com/hackHondaChallenge/O3/service/hackService.java
+++ O-3/src/main/java/com/hackHondaChallenge/O3/service/hackService.java
@@ -2,12 +2,14 @@ package com.hackHondaChallenge.O3.service;
 
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 @Service
 public interface hackService {
     public Integer compute(Integer i);
-//    public List<List<Map<String, ArrayList<Double>>>> allRoute(String start, String end);
-//    public List<List<Map<String, ArrayList<Double>>>> sortList(boolean canDrive, boolean canWalk,
-//                                                    boolean isStudent, List<List<Map<String, ArrayList<Double>>>> routes);
+    public LinkedList[] allRoute(String start, String end);
+    public LinkedList[] sort(LinkedList[] routes);
+    public void sort(int[] arr, int from_Index, int to_Index);
+    void sort(LinkedList[] arr, int from_Index, int to_Index);
 }
--- O-3/src/main/java/com/hackHondaChallenge/O3/service/hackServiceImp.java
+++ O-3/src/main/java/com/hackHondaChallenge/O3/service/hackServiceImp.java
@@ -2,6 +2,7 @@ package com.hackHondaChallenge.O3.service;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,48 +13,32 @@ public class hackServiceImp implements hackService {
     public Integer compute(Integer i) {
         return i;
     }
-//
-//    @Override
-//    public List<List<Map<String, ArrayList<Double>>>> allRoute(String start, String destination){
-//
-//        if (start == "Ohio Union" && destination == "RPAC") {
-//            // [[<"drive", [4, 0]>], [<"walk", [10, 0]>], [<"walk", [11, 0]>]]
-//            List<Map<String, ArrayList<Double>>> route_list = new ArrayList<Map<String, ArrayList<Double>>>();
-//            Map<String, ArrayList<Double>> solution_1 = new HashMap<String, ArrayList<Double>>();
-//            solution_1.put("drive", new ArrayList<Double>(Array.asList(4, 0)));
-//            Map<String, ArrayList<Double>> solution_2 = new HashMap<String, ArrayList<Double>>();
-//            solution_2.put("walk", new ArrayList<Double>(Array.asList(10, 0)));
-//            route_list.add(solution_1);
-//            route_list.add(solution_2);
-//            List<List<Map<String, ArrayList<Double>>>> route_1 = ;
-//        }
-//
-//        return route;
-//    }
-//
-//    @Override
-//    public List<List<Map<String, ArrayList<Double>>>> sortList(boolean canDrive, boolean canWalk,
-//                                                    boolean isStudent, List<List<Map<String, ArrayList<Double>>>> routes){
-//        List<List<Map<String, ArrayList<Double>>>> result = new ArrayList<>();
-//        boolean add = true;
-//
-//        next:
-//        for(int i = 0; i < routes.size(); i++){
-//            for(int j = 0; j < routes.get(i).size(); j++){
-//                add = (routes.get(i).get(j).containsKey("drive") || !canDrive);
-//                add = (routes.get(i).get(j).containsKey("walk") || !canWalk) && add;
-//                if(isStudent){
-//
-//                }
-//            }
-//            if(add){
-//                result.add(routes.get(i));
-//            }
-//        }
-//        if(isStudent){
-//
-//        }
-//
-//        return result;
-//    }
+    @Override
+    public LinkedList[] allRoute(String start, String end){
+
+    }
+    @Override
+    public LinkedList[] sort(LinkedList[] routes){
+        LinkedList[] solution = new LinkedList[routes.length];
+
+
+
+        return solution;
+    }
+    @Override
+    public void sort(LinkedList[] arr, int from_Index, int to_Index){
+        int[] importance = new int[arr.length];
+        LinkedList temp = new LinkedList();
+
+        for(int i = 0; i < arr.length; i++){
+            while(arr[i] != null){
+                importance[i] += arr[i].importance;
+                arr[i] = arr[i].next;
+            }
+        }
+        for(int i = 0; i < importance.length; i++){
+            if()
+        }
+    }
+
 }
