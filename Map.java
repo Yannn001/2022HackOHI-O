27852ffbb0b3af47df9836a19ed43eaced5709dc
anaxqx <xiong.468@osu.edu>
2022-10-08 12:44:10 -0400

    test

--- Map.java
+++ Map.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Map {
     List<List<Map.Entry<k,v>>> route(String id, boolean canWalk, boolean ){
         List<List<Map<k, v>>> allRoute = new List<>;
+        if canWalk;
         return allRoute;
     }
 
